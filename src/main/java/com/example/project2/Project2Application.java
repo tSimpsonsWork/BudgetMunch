b2062bd57cbfd9afda7b772124ac0152b0167619
Trevor <tsimp010@fiu.edu>
2024-09-16 21:07:18 -0400

    Data connected to frontend

--- src/main/java/com/example/project2/Project2Application.java
+++ src/main/java/com/example/project2/Project2Application.java
@@ -1,13 +1,18 @@
 package com.example.project2;
 
 
+import com.example.project2.entity.Response;
+import com.example.project2.entity.Result;
 import com.example.project2.entity.Student;
+import com.example.project2.entity.repository.StudentRepository;
 import com.example.project2.service.StudentService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -15,7 +20,7 @@ import java.util.Random;
 
 @SpringBootApplication
 @Slf4j
-public class Project2Application implements CommandLineRunner {
+public class Project2Application implements CommandLineRunner{
 
     private final StudentService studentService;
 
@@ -28,22 +33,25 @@ public class Project2Application implements CommandLineRunner {
 
     }
     @Override
-    public void run(String... args) throws Exception {
-        try {
-            //TODO: Students will get deleted before new so change if you want to keep
-            List<Student> listOfStudents = new ArrayList<>();
-            studentService.deleteAllStudents();
-            for (int i = 0; i < 20; i++) {
-                Random random = new Random();
-                String randomName = "Student".concat(String.valueOf(random.nextLong(1, 100)));
-                Student student = new Student(random.nextLong(1, 100), randomName, randomName.concat("@fiu.edu"), random.nextInt(18, 60));
-                listOfStudents.add(student);
-            }
-            studentService.saveStudent(listOfStudents);
-            log.info("Students successfully added");
-        }catch (Exception e){
-            log.error("Student unsuccessful added");
-            throw new Exception();
-        }
+    public void run(String... args){
+
+        studentService.addStudent2();
+
+//        try {
+//            //TODO: Students will get deleted before new so change if you want to keep
+//            List<Student> listOfStudents = new ArrayList<>();
+//            studentService.deleteAllStudents();
+//            for (int i = 0; i < 20; i++) {
+//                Random random = new Random();
+//                String randomName = "Student".concat(String.valueOf(random.nextLong(1, 100)));
+//                Student student = new Student(1L,randomName, 1.1, "Hello", random.nextInt(18, 60));
+//                listOfStudents.add(student);
+//            }
+//            studentService.saveStudent(listOfStudents);
+//            log.info("Students successfully added");
+//        }catch (Exception e){
+//            log.error("Student unsuccessful added");
+//            throw new Exception();
+//        }
     }
 }
