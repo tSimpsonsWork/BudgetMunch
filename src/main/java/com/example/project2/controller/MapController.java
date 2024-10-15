2ecb09f6a72a68d62a2a44721d9d7c275fa43628
Trevor <tsimp010@fiu.edu>
2024-10-14 21:45:36 -0400

    Controller add

--- src/main/java/com/example/project2/controller/MapController.java
+++ src/main/java/com/example/project2/controller/MapController.java
@@ -1,6 +1,8 @@
 package com.example.project2.controller;
+import com.example.project2.entity.Result;
 import com.example.project2.service.StudentService;
 import com.example.project2.entity.Response;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -21,9 +23,21 @@ public class MapController {
     }
 
     @GetMapping("/getLocation")
-    public List getGeoDetails(){
+    public List getGeoDetails() throws JsonProcessingException {
         //TODO: Make sure customer is added to the database and return the list
         Response responseBody = studentService.getGeoDetails();
+        responseBody.getResult();
+        List<Result> list = List.of(responseBody.getResult());
+        list.forEach(post->{
+            if(post.getPrice_level()==0)
+                post.setPrice_level(100);
+            else if (post.getPrice_level()==1)
+                post.setPrice_level(200);
+            else if (post.getPrice_level()==2)
+                post.setPrice_level(300);
+            else
+                post.setPrice_level(300);
+        });
         //return response.getBody();
         return List.of(responseBody.getResult());
 
