3add2d702bb3b46795d8aad66ab10453c4604400
Arelyy <arelyy@gmail.com>
2024-10-15 20:34:04 -0400

    added price ranges code for the price_level column

--- src/main/java/com/example/project2/controller/MapController.java
+++ src/main/java/com/example/project2/controller/MapController.java
@@ -1,18 +1,17 @@
 package com.example.project2.controller;
 
-import com.example.project2.entity.repository.StudentRepository;
+import com.example.project2.entity.Response;
 import com.example.project2.entity.Result;
 import com.example.project2.entity.Student;
+import com.example.project2.entity.repository.StudentRepository;
 import com.example.project2.service.StudentService;
-import com.example.project2.entity.Response;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 
 
 @RestController
@@ -27,13 +26,13 @@ public class MapController {
     @Autowired
     private StudentRepository studentRepository;
 
-//    @Autowired
+    //    @Autowired
 //    public MapController(StudentService budget2Service, JsonParser jsonParser) {
 //        this.studentService = budget2Service;
 //        this.jsonParser = jsonParser;
 //    }
     @Autowired
-    public MapController(StudentService studentService, StudentRepository studentRepository){
+    public MapController(StudentService studentService, StudentRepository studentRepository) {
         this.studentService = studentService;
         this.studentRepository = studentRepository;
     }
@@ -64,34 +63,53 @@ public class MapController {
     }
 
     @GetMapping("/check-username/{username}")
-    public ResponseEntity<Boolean> checkUsername(@PathVariable String username){
+    public ResponseEntity<Boolean> checkUsername(@PathVariable String username) {
         boolean exists = studentService.existsByUsername(username);
         return ResponseEntity.ok(exists);
     }
 
 
     @GetMapping("/getLocation")
-
-    public List getGeoDetails() throws JsonProcessingException {
-        //TODO: Make sure customer is added to the database and return the list
+    public List<Map<String, Object>> getGeoDetails() throws JsonProcessingException {
         Response responseBody = studentService.getGeoDetails();
-        responseBody.getResult();
-        List<Result> list = List.of(responseBody.getResult());
-        list.forEach(post->{
-            if(post.getPrice_level()==0)
-                post.setPrice_level(100);
-            else if (post.getPrice_level()==1)
-                post.setPrice_level(200);
-            else if (post.getPrice_level()==2)
-                post.setPrice_level(300);
-            else
-                post.setPrice_level(300);
-        });
-        //return response.getBody();
-        return List.of(responseBody.getResult());
+        List<Result> resultList = List.of(responseBody.getResult());
 
+        // Create a new list to store the mapped results
+        List<Map<String, Object>> mappedResults = new ArrayList<>();
+
+        // Loop through each result and map the price level to a string range
+        resultList.forEach(post -> {
+            Map<String, Object> resultMap = new HashMap<>();
+            resultMap.put("name", post.getName());
+            resultMap.put("rating", post.getRating());
+            resultMap.put("vicinity", post.getVicinity());
+
+            // Map price level to string range
+            String priceLevelString;
+            switch (post.getPrice_level()) {
+                case 0:
+                    priceLevelString = "$1-$10";
+                    break;
+                case 1:
+                    priceLevelString = "$10-$25";
+                    break;
+                case 2:
+                    priceLevelString = "$25-$45";
+                    break;
+                default:
+                    priceLevelString = "$50+";
+                    break;
+            }
+            resultMap.put("price_level", priceLevelString); // Add the string range to the result map
+
+            mappedResults.add(resultMap); // Add the modified result to the list
+        });
+
+        return mappedResults; // Return the modified list
     }
-    //register new user
-    //inside controller class, do responsebody.getResults, store it as a list.of, iterate the list
 }
+
+//register new user
+    //inside controller class, do responsebody.getResults, store it as a list.of, iterate the list
+
 
